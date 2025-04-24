f67f65a33efb68312bbe4280b186d89d01412b2d
Duran Enzo <enzoduran828@gmail.com>
2025-04-23 22:42:08 -0300

    Terminao

--- Desafio2/Program.cs
+++ Desafio2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int cantidad;
+Console.WriteLine("ingrese de donde arrancar la piramide");
+cantidad=int.Parse(Console.ReadLine());
+Console.WriteLine(" ");
+for (int i = 1; i <= cantidad; i++)
+{
+    Console.SetCursorPosition((Console.WindowWidth - cantidad) / 2, Console.CursorTop);
+    for (int j = cantidad - i; j > 0; j--)
+    {
+        Console.Write(" ");
+    }
+    for (int j = 1; j < i; j++)
+    {
+        Console.Write("* ");
+    }
+    Console.WriteLine("*");
+}
+Console.ReadKey();
